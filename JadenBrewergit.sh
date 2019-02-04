a8df4442df48f8947d4993a3441484923ee02a79
= <=>
2019-02-04 12:18:44 -0800

    AA

---
+++ JadenBrewergit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  JadenBrewer;
+git config --global user.name  JadenBrewer;
+git config --global user.user JadenBrewer;
+git config --global user.email jaden.brewer@ksd.org;
+git config --list;
